7e1dccc144110afc329f25acba1c524c02b0a187
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-05-07 16:23:28 +0200

    first commit

---
+++ Main.kt
@@ -0,0 +1,9 @@
+package converter
+
+fun main() {
+    println("145 centimeters is 1.45 meters\n" +
+            "2 miles is 3.2187 kilometers\n" +
+            "5.5 inches is 139.7 millimeters\n" +
+            "12 degrees Celsius is 53.6 degrees Fahrenheit\n" +
+            "3 pounds is 1.360776 kilograms")
+}
