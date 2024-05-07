fb2a208e22d0adb4e23cbd14823da993a63a36a8
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-05-07 16:36:57 +0200

    kilometer

--- Main.kt
+++ Main.kt
@@ -1,9 +1,20 @@
 package converter
 
 fun main() {
-    println("145 centimeters is 1.45 meters\n" +
-            "2 miles is 3.2187 kilometers\n" +
-            "5.5 inches is 139.7 millimeters\n" +
-            "12 degrees Celsius is 53.6 degrees Fahrenheit\n" +
-            "3 pounds is 1.360776 kilograms")
+    print("Enter a number and a measure: ")
+    val (a, b) = readln().split(" ")
+    val measure = b.lowercase()
+    val number = try {
+        a.toInt()
+    } catch (e: NumberFormatException) {
+        println("Wrong input")
+        return
+    }
+    if (number == 1 && measure == "km" || measure == "kilometer") {
+        println("1 kilometer is 1000 meters")
+    } else if (measure == "km" || measure == "kilometers") {
+        println("$number kilometers is ${number}000 meters")
+    } else {
+        println("Wrong input")
+    }
 }
